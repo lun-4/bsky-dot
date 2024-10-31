c13e2bdf634a2739d42343d1612f8acabe30489e
Luna <git@l4.pm>
2024-10-30 23:06:06 -0300

    add processing via HF pipeline

--- testrunner.py
+++ testrunner.py
@@ -3,6 +3,9 @@ from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
+from flask import Flask, request
+
+app = Flask(__name__)
 
 
 # Preprocess text (username and link placeholders)
@@ -16,29 +19,64 @@ def preprocess(text):
 
 
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
-config = AutoConfig.from_pretrained(MODEL)
-# PT
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-# model.save_pretrained(MODEL)
-text = "Covid cases are increasing fast!"
-text = preprocess(text)
-encoded_input = tokenizer(text, return_tensors="pt")
-output = model(**encoded_input)
-scores = output[0][0].detach().numpy()
-scores = softmax(scores)
-# # TF
-# model = TFAutoModelForSequenceClassification.from_pretrained(MODEL)
-# model.save_pretrained(MODEL)
-# text = "Covid cases are increasing fast!"
-# encoded_input = tokenizer(text, return_tensors='tf')
-# output = model(encoded_input)
-# scores = output[0][0].numpy()
-# scores = softmax(scores)
-# Print labels and scores
-ranking = np.argsort(scores)
-ranking = ranking[::-1]
-for i in range(scores.shape[0]):
-    l = config.id2label[ranking[i]]
-    s = scores[ranking[i]]
-    print(f"{i+1}) {l} {np.round(float(s), 4)}")
+#tokenizer = AutoTokenizer.from_pretrained(MODEL)
+#config = AutoConfig.from_pretrained(MODEL)
+#model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+from transformers import pipeline
+sentiment_task = pipeline("sentiment-analysis", model=MODEL, tokenizer=MODEL)
+
+@app.get("/v1/models")
+def models():
+    return {
+      "object": "list",
+      "data": [
+        {
+          "id": "cardiffnlp/twitter-roberta-base-sentiment-latest",
+        }
+      ]
+    }
+
+def _old(j):
+    text = j['text']
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors="pt")
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    ranking = np.argsort(scores)
+    ranking = ranking[::-1]
+    max_label, max_score = None, 0
+
+    for i in range(scores.shape[0]):
+        l = config.id2label[ranking[i]]
+        s = scores[ranking[i]]
+        if max_label is None:
+            max_label = l
+            max_score = s
+        if s > max_score:
+            max_label = l
+            max_score = s
+        print(f"{i+1}) {l} {np.round(float(s), 4)}")
+    return max_label,max_score
+
+@app.post("/api/v1/sentiment")
+def compute_sentiment():
+    j = request.get_json()
+    print(j['text'])
+    output = sentiment_task(j['text'])
+    print(output)
+
+    max_label, max_score = None, 0
+    for it in output:
+        l, s = it['label'], it['score']
+        if max_label is None:
+            max_label = l
+            max_score = s
+        if s > max_score:
+            max_label = l
+            max_score = s
+
+    return {
+        'output': {'predictions': [{'label': max_label, 'score': max_score}]}
+    }
