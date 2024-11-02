db8ec986cb8f66dd2d9b5553596a2671e4a479ed
Luna <git@l4.pm>
2024-11-02 17:31:15 -0300

    make dot_v2 forget previous sentiments and tweak epsilon

--- dot_v2.go
+++ dot_v2.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/samber/lo"
 )
 
+type DotV2Sentiment struct {
+	timestamp int64  `json:"t"`
+	value     string `json:"v"`
+}
+
 type DotV2 struct {
-	d              float64
-	lastSentiments []string
+	d              float64          `json:"d"`
+	lastSentiments []DotV2Sentiment `json:"s"`
 }
 
 type DotVwState float64
 
 func NewDotV2(previousState map[string]any) DotV2 {
-	sentimentsStrArray := make([]string, 0)
+	sentimentsStrArray := make([]DotV2Sentiment, 0)
 
 	for _, sentimentAny := range previousState["s"].([]any) {
-		sentimentsStrArray = append(sentimentsStrArray, sentimentAny.(string))
+		sentimentMap := sentimentAny.(map[string]any)
+		sentimentsStrArray = append(sentimentsStrArray, DotV2Sentiment{
+			timestamp: sentimentMap["t"].(int64),
+			value:     sentimentMap["v"].(string),
+		})
 	}
 	return DotV2{
 		d:              previousState["d"].(float64),
@@ -28,14 +38,21 @@ func NewDotV2(previousState map[string]any) DotV2 {
 func NewEmptyDotV2() DotV2 {
 	return DotV2{
 		d:              0.0,
-		lastSentiments: make([]string, 0),
+		lastSentiments: make([]DotV2Sentiment, 0),
 	}
 }
 
 func (d *DotV2) Serialize() map[string]any {
+	asSentimentMap := make([]map[string]any, 0)
+	for _, s := range d.lastSentiments {
+		asSentimentMap = append(asSentimentMap, map[string]any{
+			"t": s.timestamp,
+			"v": s.value,
+		})
+	}
 	return map[string]any{
 		"d": d.d,
-		"s": lo.ToAnySlice(d.lastSentiments),
+		"s": lo.ToAnySlice(asSentimentMap),
 	}
 }
 
@@ -46,7 +63,7 @@ func (d *DotV2) Version() string {
 	return "v2"
 }
 
-func appendToMaxLen(array []string, maxLen int, value string) []string {
+func appendToMaxLen(array []DotV2Sentiment, maxLen int, value DotV2Sentiment) []DotV2Sentiment {
 	if len(array) >= maxLen {
 		array = array[1:]
 	}
@@ -63,9 +80,15 @@ func (d *DotV2) Value() float64 {
 
 func (d *DotV2) lastWinningSentiment() string {
 
+	now := time.Now().Unix()
+
 	winCounter := make(map[string]int)
 	for _, lastSentiment := range d.lastSentiments {
-		winCounter[lastSentiment]++
+		delta := now - lastSentiment.timestamp
+		if delta > 20*60 {
+			continue
+		}
+		winCounter[lastSentiment.value]++
 	}
 
 	var winningSentiment string
@@ -91,28 +114,27 @@ func (d *DotV2) Forward(sentiments []string) error {
 		proportions[sentiment] = proportion
 	}
 
-	epsilon := 0.005 // a small value to increase/decrease the dot on each time step
+	epsilonIncrease := 0.02
+	epsilonDecrease := 0.007
 	var winningSentiment string
 	for sentiment, proportion := range proportions {
-		// NOTE this is a very special magical number whose tweaking leads to collapses on
-		// either side of the dot spectrum (either everyone stays at 0 because no sentiment can breach the threshold,
-		// or everyone's a 1 because a sentiment wins at every timestamp)
-		if proportion > 0.405 {
-			// the network is converging itself towards a common goal, increase dot by epsilon
+		if proportion > 0.35 {
 			winningSentiment = sentiment
 		}
 	}
 
-	lastWinningSentiment := d.lastWinningSentiment()
-	currentSentimentStillWins := lastWinningSentiment == winningSentiment
-	d.lastSentiments = appendToMaxLen(d.lastSentiments, 60, winningSentiment)
-
-	if currentSentimentStillWins {
-		d.d = limitDot(d.d + epsilon)
+	fmt.Println("wins", winningSentiment)
+	if winningSentiment == "positive" || winningSentiment == "negative" {
+		winningSentimentPreviousRun := d.lastWinningSentiment()
+		currentSentimentStillWins := winningSentimentPreviousRun == winningSentiment
+		d.lastSentiments = appendToMaxLen(d.lastSentiments, 60, DotV2Sentiment{timestamp: time.Now().Unix(), value: winningSentiment})
+		if currentSentimentStillWins {
+			d.d = limitDot(d.d + epsilonIncrease)
+		}
 		return nil
 	} else {
 		// no convergence, decrease by epsilon
-		d.d = limitDot(d.d - epsilon)
+		d.d = limitDot(d.d - epsilonDecrease)
 		return nil
 	}
 }
